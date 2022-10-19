77bfab3c894d78b305edd2cb7ef96f226196ef5a
Katkauske <kkatkauske@gmail.com>
2022-10-19 09:39:03 +0300

    String methods uzduotys viename aplanke

---
+++ uzduotis.js
@@ -0,0 +1,14 @@
+
+
+
+
+let zodis = prompt("Iveskite zodi ar zodziu jungini.");
+let ieskomaGalune = prompt("Iveskite ieskoma galune!");
+let ieskomasZodis = "taip";
+alert(zodis.length);
+
+console.log(zodis.at(-1));
+console.log(zodis.includes(ieskomasZodis));
+console.log(zodis.endsWith (ieskomaGalune));
+
+
