99c5ea95e70ff980c4ffa8a6c3626f4858000891
Katkauske <kkatkauske@gmail.com>
2022-10-19 09:52:24 +0300

    Pirma uzduotis

---
+++ pirma.js
@@ -0,0 +1,16 @@
+//rkite funkciją, kuri patikrina ar du žodžiai yra vienodi ignoruojant mažųjų ir didžiųjų raidžių skirtumą. 
+//Išbandykite ją pagrindinėje programoje.
+
+let zodis1 = "KoMPIuTEris";
+let zodis2 = "kompIUTtEris";
+console.log(arVienodiNepaisantDydziu(zodis1, zodis2))
+
+function arVienodiNepaisantDydziu(zodis1,zodis2)  {
+    if (zodis1 == zodis2) {
+        return "Zodziai lygus nepaisant simboliu dydziu skirtumu."
+    }
+    else {
+        return "Net ir nepaisant simboliu dydziu skirtumu zodziai nera lygus."
+    }
+
+}
